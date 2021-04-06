71df1345c66ad7a3697b9b044479255e58cf2903
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-04-07 01:00:52 +0300

    adding a lot of information and style also a lot of question

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # About-Me
-Some information about me
+
+**Some information about me**
+
+Hello visitor, if you are interested in knowing some details about me related to my educational life and my experiences, you can go to my page here
--- javascript/app.js
+++ javascript/app.js
@@ -2,17 +2,20 @@
 
 alert("Welcome to my Information");
 
-let welcome = prompt("What is your name");
+let guestname = prompt("What is your name");
 
-alert("Your welcome " + welcome);
+alert("Your welcome " + guestname);
 
-// question 5
+
+let score = 0;
+// question 1
 let question1 = prompt(" Are you excited of my information ", " Yes or No ");
 question1 = question1.toLowerCase();
 switch (question1) {
   case "yes":
   case "y":
     alert(" Great ");
+    score++
     break;
 
   case "no":
@@ -24,6 +27,7 @@ switch (question1) {
     alert("Please enter an answer next time thank you :) ");
 
     alert(" Thank you ");
+
     break;
 }
 
@@ -34,6 +38,7 @@ switch (question2) {
   case "yes":
   case "y":
     alert(" Great ");
+    score++
     break;
 
   case "no":
@@ -49,12 +54,13 @@ switch (question2) {
 }
 
 // question 3
-let question3 = prompt(" Are you love codeing ", " Yes or No ");
+let question3 = prompt(" Are you love coding ", " Yes or No ");
 question3 = question3.toLowerCase();
 switch (question3) {
   case "yes":
   case "y":
     alert(" Great ");
+    score++
     break;
 
   case "no":
@@ -76,11 +82,12 @@ switch (question4) {
   case "yes":
   case "y":
     alert(" Great ");
+    score++
     break;
 
   case "no":
   case "n":
-    alert(" Ok As you like :) ");
+    alert(" That's cool ");
     break;
 
   case "":
@@ -97,6 +104,7 @@ switch (question5) {
   case "yes":
   case "y":
     alert(" Great ");
+    score++
     break;
 
   case "no":
@@ -111,4 +119,96 @@ switch (question5) {
     break;
 }
 
-alert("Thank you for your visting " + welcome);
+// question 6
+let question6 = prompt("How many user vist my web site do you think until now? ", " Make sure to insert Number only ");
+
+for (let i = 3; i > 0; i--) {
+  question6 = parseInt(question6);
+
+  if (question6 === 50) {
+    score++;
+    alert('Great your answer is correct' + ' your final score until now is ' + score);
+
+
+    break;
+
+  }
+  else if (question6 < 40) {
+    alert('Too low! you have ' + i + ' attempts left');
+    question6 = prompt('Try to repeat your answer with a better guess ');
+    if (i == 1) {
+      alert('Sorry your answe not correct but still good dont worry ' + ' your final score on my information is ' + score);
+
+    }
+  }
+  else if (question6 > 60) {
+    alert('Too high! you have ' + i + ' attempts left');
+    question6 = prompt('Try to repeat your answer with a better guess ');
+    if (i == 1) {
+      // alert('Sorry your answer not correct but still good do not worry' + ' your final score on my information is ' + score);
+
+    }
+  }
+
+  else if (question6 > 40) {
+    alert('So close! you have ' + i + ' attempts');
+    question6 = prompt('Try to repeat your answer with a better guess ');
+    if (i == 1) {
+      alert('Sorry your answer not correct but still good do not worry' + ' your final score on my information is ' + score);
+
+    }
+  }
+  else if (question6 < 60) {
+    alert('So close! you have ' + i + ' attempts');
+    question6 = prompt('Try to repeat your answer with a better guess ');
+    if (i == 1) {
+      alert('Sorry your answer not correct but still good do not worry' + ' your final score on my information is ' + score);
+
+    }
+  }
+}
+
+
+let Bestofme = ['sport', 'traveling', 'hanging out', 'coding'];
+let answer = prompt('Can you guess some of my personal interests?', 'love life, playing');
+answer = answer.toLowerCase();
+for (let i = 5; i > 0; i--) {
+
+  if (answer == Bestofme[0]) {
+    score++;
+    alert('You do it congratulation, your answer right, you pick one of my interest correctly' + ' your final score on my information is ' + score);
+
+    break;
+
+
+  }
+  else if (answer == Bestofme[1]) {
+    score++;
+    alert('You do it congratulation, your answer right, you pick one of my interest correctly' + ' your final score on my information is ' + score);
+
+    break;
+
+  }
+  else if (answer == Bestofme[2]) {
+    score++;
+    alert('You do it congratulation, your answer right, you pick one of my interest correctly' + ' your final score on my information is ' + score);
+
+    break;
+
+  }
+
+  else if (answer == Bestofme[3]) {
+    score++;
+    alert('You do it congratulation, your answer right, you pick one of my interest correctly' + ' your final score on my information is ' + score);
+
+    break;
+
+  }
+  else {
+
+    alert('So close! you have ' + i + ' attempts');
+  }
+}
+
+
+alert("Thank you for your time " + guestname);
